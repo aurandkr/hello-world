4f848023c5005420d1eaccf098c0ea208f4bea6f
aurandkr <46753101+aurandkr@users.noreply.github.com>
2019-01-16 10:09:33 -0500

    Lab 1 Submission

---
+++ Hello.java
@@ -0,0 +1,26 @@
+/***********************************
+* Java Program Name
+************************************
+*Project/Program Description
+*___________________________________
+*Kaleb Aurand
+*January 16, 2019
+*CMSC 255 Section 003
+************************************/
+
+public class Hello {
+	public static void printHeading(){
+		System.out.println();
+		System.out.println("Kaleb Aurand");
+		System.out.println("Hello.java");
+		System.out.println("Lab 1, Hello World Beginner Program");
+		System.out.println("January 16, 2019");
+		System.out.println("CMSC 255 Section 003");
+		System.out.println();
+	}
+	
+	public static void main(String [] args) {
+		printHeading();
+		System.out.println("	Hello");
+	}
+}
